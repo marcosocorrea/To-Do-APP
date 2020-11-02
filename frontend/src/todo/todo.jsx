5357a51ea7ffa3196a33f2d71a9f7fd318946a74
MARCOS DE OLIVEIRA CORREA <marcosocorrea@gmail.com>
2020-11-02 16:16:32 -0300

    02 11 2020

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,16 +1,54 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+const URL = 'http://localhost:3003/api/todos'
+
 export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+
+        this.refresh()
+    }
+
+    refresh() {
+        axios.get(`${URL}?sort=-createdAt`)
+            .then (resp => this.setState({...this.state, description: '', list: resp.data }))
+    }
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value })
+    }
+
+    handleAdd() {
+        const description = this.state.description
+        axios.post(URL, { description })
+            .then(resp => this.refresh())
+    }
+
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
-                <TodoForm/>
-                <TodoList/>
+
+                <TodoForm handleAdd={this.handleAdd} description={this.state.description} handleChange={this.handleChange}/>
+
+                <TodoList list={this.state.list} handleRemove={this.handleRemove}/>
             </div>
         )
     }
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default props => (
-    <div>
-        <h1>list</h1>
-    </div>
-)
+export default props => {
+
+    const renderRows = () => {
+        const list = props.list || []
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td>{todo.description}</td>
+                <td>
+
+                    <IconButton style='success' icon='check' onClick={() => props.handleMarkAsDone(todo)} />
+
+                    <IconButton style='warning' icon='undo' onClick={() => props.handleMarkAsPending(todo)} />
+
+                    <IconButton style='danger' icon='trash-o' onClick={() => props.handleRemove(todo)} />
+                    
+                </td>
+            </tr>
+        ))
+    }
+
+    return (
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
